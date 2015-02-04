ec3128f634d2af6d8b74de22f9cc92ff5675ac20
ishan-marikar <ishan.marikar@outlook.com>
2015-02-04 11:16:26 +0530

    new stuff

--- App/submit_cart.php
+++ App/submit_cart.php
@@ -6,6 +6,8 @@ if(isset($_POST['userId']) && isset($_POST['cartString']))
   $user_id = $_POST['userId'];
 	$cart_string = $_POST['cartString'];
   $cartData = json_decode($cart_string, true);
+  $shoppingCart->getUserCart($user_id);
+  
   foreach ($cartData as $item) {
       $shoppingCart->addToCart($item['itemID'], $item['itemQty']);
       //also part of the array, just remember = $item['itemTotal']
